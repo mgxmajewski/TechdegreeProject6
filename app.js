0df50b54a10ce6e520a964f414787dd25c336415
mgxmajewski <mgxmajewski@BIVTR0M.localdomain>
2021-03-07 23:07:08 +0100

    Set public route

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const { projects } = require('../data/data.json');
 const app = express;
+
 app.set('view engine', 'pug');
+app.use('/public', express.static(path.json(__dirname, 'public')));
+
+
