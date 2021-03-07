caaeb9810e6038aa0c58d64d6f7477420b607906
mgxmajewski <mgxmajewski@BIVTR0M.localdomain>
2021-03-08 00:10:27 +0100

    Set up localhost on port 3000 and root

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const { projects } = require('../data/data.json');
-const app = express;
+const { projects } = require('./data/data.json');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'pug');
-app.use('/public', express.static(path.json(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.set("view engine", "pug");
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
 
