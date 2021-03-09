4b87a071624677b9da0af303f4d3752efe907822
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-09 21:17:49 +0100

    Fix bug related to deleting next argument It's necessary even when not used explicitly

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const createError = require('http-errors');
-const logger = require('morgan');
+// const logger = require('morgan');
 
 app.set('views', path.join(__dirname, 'views'));
 
 app.set("view engine", "pug");
 app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -25,7 +25,7 @@ app.get("/about", (req, res) => {
 });
 
 
-app.get('/project/:id', function(req, res) {
+app.get('/project/:id', function(req, res, next ) {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId );
         res.render('project', {project} );
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 });
 
 // Handles errors
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     // Ensure the err object has status and message properties defined
     err.status = err.status || 500;
     err.message = err.message || 'Server error';
