694964e710e5146075f03f4f7816d2a8420b5866
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-03-19 21:10:18 +0100

    Add config for Cloud Code development and deployment to GCP.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM node:16.13-alpine3.14
+FROM node:17-slim
 
 # Create app directory
 WORKDIR /usr/src/app
 
 COPY package*.json ./
 
-RUN npm install
-COPY . .
-EXPOSE 3000
+RUN npm install --production
+COPY . ./
+
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { projects } = require('./data/data.json');
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 const createError = require('http-errors');
 
 // Set view engine and path
@@ -50,8 +50,10 @@ app.use((err, req, res, next) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(
+    `Hello from Cloud Run! The container started successfully and is listening for HTTP requests on ${PORT}`
+  );
 })
 
 
